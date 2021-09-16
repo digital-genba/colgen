4f6c01b9f4eff877231666e674df86846852bd2d
Digital Genba <dev@digitalgenba.net>
2021-09-16 18:22:10 +1000

    Initial colorPairFromBaseByContrast function

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import _ from 'lodash/fp';
+import {
+  Theme,
+  Color,
+  BackgroundColor
+} from '@adobe/leonardo-contrast-colors';
+
+export const colorPairFromBaseByContrast = (
+  requiredContrast,
+  baseColor,
+) => {
+  const contrastFromCentre = _.ceil(Math.sqrt(
+    Math.abs(requiredContrast)
+  ), 2);
+
+  const base = new Color({
+    name: 'base',
+    colorKeys: [baseColor],
+    ratios: [contrastFromCentre, -1 * contrastFromCentre],
+    colorspace: 'RGB'
+  });
+
+  const theme = new Theme({
+    colors: [base],
+    output: 'HEX',
+    backgroundColor: new BackgroundColor({
+      name: 'blank',
+      colorKeys: ['#000000'],
+      ratios: [],
+    }),
+    lightness: 50,
+  });
+
+  return theme.contrastColorValues;
+};
+
+export default colorPairFromBaseByContrast;
